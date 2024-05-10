558c246f1e2c3bdbae4b3dc68c7d22dd66c470b0
unknown <belasalah@gmail.com>
2024-05-10 18:53:27 +0200

    save th egame

---
+++ app/dashboard/articles/page.tsx
@@ -0,0 +1,6 @@
+export default function Articles() {
+
+    return(
+        <h1>Articles listener</h1>
+    )
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+export default function Invoices() {
+    return(
+        <h1>Custoùers Invoices ..</h1>
+    )
+}
